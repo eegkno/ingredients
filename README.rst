ec1caff5dae1503ca1517b49391b7de58c8b5ff8
Edgar Garcia Cano <eegkno@gmail.com>
2020-05-02 00:14:40 -0400

    first commit

---
+++ README.rst
@@ -0,0 +1,24 @@
+A lot of ingredients
+==============================
+
+Ingredients for everything
+
+
+Usage
+-----
+
+Installation
+------------
+
+Requirements
+------------
+
+Compatibility
+-------------
+
+Licence
+-------
+
+Authors
+-------
+
---
+++ setup.py
@@ -0,0 +1,23 @@
+"""Setup file for ingredients."""
+
+import pathlib
+import setuptools
+
+
+current_dir = pathlib.Path(__file__).parent
+
+readme = (current_dir / "README.md").read_text()
+
+setuptools.setup(
+    name="ingredients",
+    version="0.0.1",
+    description="A lot of ingredients",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/eegkno/ingredients.git",
+    author="Edgar Garcia Cano",
+    author_email="eegkno@gmail.com",
+    classifiers=["Programming Language :: Python"],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
+)
